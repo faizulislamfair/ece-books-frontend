f075f999ba028e25678049390dd4f28467ac7c59
faizulislamfair <faizulislamfair@gmail.com>
2022-12-11 23:44:12 +0600

    up-down arrow, text-under image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import './Home.css';
 import main_image from './../../assets/images/main_image.svg';
+import under_arrow from './../../assets/images/under_arrow.png';
+import text_under from './../../assets/images/text_under.svg';
+
 
 const Home = () => {
     return (
         <div className="main-section container">
-            <button className='main-button btn'>ECE Books</button>
+            <button className='main-button btn'>ECE Books</button> <br />
+
+            <img className='text-merge' src={text_under} alt="" />
             <div className="main-text">
-                Download all of your academic book pdf from one place without any hustle!
+                Download all of your academic book pdf from one place <br />
+                without any hassle!
             </div>
+
+
+            <button className='btn all-books'>Download Books</button>
+
             <div className="image-section">
                 <img src={main_image} alt="" />
             </div>
+
+            <div className='up-down mt-5'>
+                <img src={under_arrow} alt="" />
+            </div>
+
+            <div className="book-section">
+                <div>
+                    kola
+                </div>
+                <div>
+                    kola
+                </div>
+                <div>
+                    kola
+                </div>
+                <div>
+                    kola
+                </div>
+            </div>
+
         </div>
     );
 };
