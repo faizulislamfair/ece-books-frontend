a1708cd03b67c24a3e0632d1f4cb76f18be70967
faizulislamfair <faizulislamfair@gmail.com>
2022-12-25 00:46:26 +0600

    footer section

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Footer.css';
+import balloon from './../../assets/images/balloons.svg';
+import party_papers from './../../assets/images/party_papers.svg';
+import copyright_symbol from './../../assets/images/copyright_symbol.svg';
+import text_small_merge from './../../assets/images/text_small_under.svg';
+
+
+
+const Footer = () => {
+    return (
+        <div>
+            <img className='text-small-merge' src={text_small_merge} alt="" />
+            <div className='container-fluid footer-section'>
+                <img src={balloon} alt="" />
+                <div>
+                    <div className='footer-text-thanks'>
+                        Special Thanks
+                    </div>
+                    <div className='footer-text-names'>
+                        Md. Yusuf Mehemud (ECE'17) <br />
+                        Mohiuddin Al Faruki Akib (ECE'17)
+                    </div>
+                </div>
+                <img src={party_papers} alt="" />
+            </div>
+
+            <div className='copyright-section'>
+                <img src={copyright_symbol} alt="" />
+                <div className='copyright-text'>
+                    2022 All Rights Reserved - Sajid, Fair & Turag
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,65 +7,72 @@ import button_arrow from './../../assets/images/button_arrow.svg';
 import reverse_doodle from './../../assets/images/reverse_doodle.svg';
 import { Link } from 'react-router-dom';
 import Contribute from './../Contribute/Contribute';
+import Footer from './../Footer/Footer';
 
 
 const Home = () => {
     return (
-        <div className="main-section container">
-            <button className='main-button btn'>ECE Books</button> <br />
+        <div>
+            <div className="main-section container">
+                <button className='main-button btn'>ECE Books</button> <br />
 
-            <img className='text-merge' src={text_under} alt="" />
-            <div className="main-text">
-                Download all of your academic book pdf from one place <br />
-                without any hassle!
-            </div>
-
-
-            <button className='btn all-books'>Download Books</button> <br />
+                <img className='text-merge' src={text_under} alt="" />
+                <div className="main-text">
+                    Download all of your academic book pdf from one place <br />
+                    without any hassle!
+                </div>
 
 
+                <button className='btn all-books'>Download Books</button> <br />
 
 
-            <div className="image-section">
-                <img src={main_image} alt="" />
-            </div>
-
-            <div className='up-down mt-5'>
-                <img src={under_arrow} alt="" />
-            </div>
 
 
-            <img className='reverse-doodle' src={reverse_doodle} alt="" />
+                <div className="image-section">
+                    <img src={main_image} alt="" />
+                </div>
+
+                <div className='up-down mt-5'>
+                    <img src={under_arrow} alt="" />
+                </div>
 
 
-            <div className="book-section mb-5">
-                <div>
-                    <div className='rectangle rect-gap'>
+                <img className='reverse-doodle' src={reverse_doodle} alt="" />
+
+
+                <div className="book-section mb-5">
+                    <div>
+                        <div className='rectangle rect-gap'>
+                        </div>
+                        <div className='rectangle'>
+                        </div>
                     </div>
-                    <div className='rectangle'>
+                    <div className='text-flex'>
+                        <div className='text-start download-text'>
+                            Select your preferred year <br />
+                            & semester
+                        </div>
+                    </div>
+                    <div className='btn-box'>
+                        <Link style={{ textDecoration: 'none' }} to={`/one_one`}><button className='each-button'>1st year Odd semester &nbsp;&nbsp;&nbsp;<img src={button_arrow} alt="" /></button></Link>
+                        <Link style={{ textDecoration: 'none' }} to={`/two_one`}><button className='each-button'>2nd year Odd semester &nbsp;&nbsp;&nbsp;<img src={button_arrow} alt="" /></button></Link>
+                        <Link style={{ textDecoration: 'none' }} to={`/three_one`}><button className='each-button'>3rd year Odd semester &nbsp;&nbsp;&nbsp;<img src={button_arrow} alt="" /></button></Link>
+                        <Link style={{ textDecoration: 'none' }} to={`/four_one`}><button className='last-button'>4th year Odd semester &nbsp;&nbsp;&nbsp;<img src={button_arrow} alt="" /></button></Link>
+                    </div>
+                    <div className='btn-box'>
+                        <Link style={{ textDecoration: 'none' }} to={`/one_two`}><button className='each-button'>1st year Even semester &nbsp;&nbsp;&nbsp;<img src={button_arrow} alt="" /></button></Link>
+                        <Link style={{ textDecoration: 'none' }} to={`/two_two`}><button className='each-button'>2nd year Even semester &nbsp;&nbsp;&nbsp;<img src={button_arrow} alt="" /></button></Link>
+                        <Link style={{ textDecoration: 'none' }} to={`/three_two`}><button className='each-button'>3rd year Even semester &nbsp;&nbsp;&nbsp;<img src={button_arrow} alt="" /></button></Link>
+                        <Link style={{ textDecoration: 'none' }} to={`/four_two`}><button className='last-button'>4th year Even semester &nbsp;&nbsp;&nbsp;<img src={button_arrow} alt="" /></button></Link>
                     </div>
                 </div>
-                <div className='text-flex'>
-                    <div className='text-start download-text'>
-                        Select your preferred year <br />
-                        & semester
-                    </div>
-                </div>
-                <div className='btn-box'>
-                    <Link style={{ textDecoration: 'none' }} to={`/one_one`}><button className='each-button'>1st year Odd semester &nbsp;&nbsp;&nbsp;<img src={button_arrow} alt="" /></button></Link>
-                    <Link style={{ textDecoration: 'none' }} to={`/two_one`}><button className='each-button'>2nd year Odd semester &nbsp;&nbsp;&nbsp;<img src={button_arrow} alt="" /></button></Link>
-                    <Link style={{ textDecoration: 'none' }} to={`/three_one`}><button className='each-button'>3rd year Odd semester &nbsp;&nbsp;&nbsp;<img src={button_arrow} alt="" /></button></Link>
-                    <Link style={{ textDecoration: 'none' }} to={`/four_one`}><button className='last-button'>4th year Odd semester &nbsp;&nbsp;&nbsp;<img src={button_arrow} alt="" /></button></Link>
-                </div>
-                <div className='btn-box'>
-                    <Link style={{ textDecoration: 'none' }} to={`/one_two`}><button className='each-button'>1st year Even semester &nbsp;&nbsp;&nbsp;<img src={button_arrow} alt="" /></button></Link>
-                    <Link style={{ textDecoration: 'none' }} to={`/two_two`}><button className='each-button'>2nd year Even semester &nbsp;&nbsp;&nbsp;<img src={button_arrow} alt="" /></button></Link>
-                    <Link style={{ textDecoration: 'none' }} to={`/three_two`}><button className='each-button'>3rd year Even semester &nbsp;&nbsp;&nbsp;<img src={button_arrow} alt="" /></button></Link>
-                    <Link style={{ textDecoration: 'none' }} to={`/four_two`}><button className='last-button'>4th year Even semester &nbsp;&nbsp;&nbsp;<img src={button_arrow} alt="" /></button></Link>
-                </div>
+
+                <Contribute></Contribute>
+
             </div>
 
-            <Contribute></Contribute>
+            <Footer></Footer>
+
 
         </div>
     );
